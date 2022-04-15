753dcf181f3e07a3f920114c339533bed3a1ac7f
Anowarul Kabir <akabir0101@gmail.com>
2022-04-15 12:14:34 -0400

    updated data and hopper job script

--- jobs/on_hopper.sh
+++ jobs/on_hopper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=prot
-#SBATCH --output=/scratch/akabir4/scop_classification/outputs/argo_logs/prot-%N-%j.out
-#SBATCH --error=/scratch/akabir4/scop_classification/outputs/argo_logs/prot-%N-%j.err
+#SBATCH --job-name=scop
+#SBATCH --output=/scratch/akabir4/scop_classification/outputs/argo_logs/scop-%j.out
+#SBATCH --error=/scratch/akabir4/scop_classification/outputs/argo_logs/scop-%j.err
 #SBATCH --mail-user=<akabir4@gmu.edu>
 #SBATCH --mail-type=BEGIN,END,FAIL
 
@@ -10,7 +10,7 @@
 #SBATCH --cpus-per-task=8                   # Request n   cores per node
 ##SBATCH --nodes=1                          # Request N nodes
 #SBATCH --mem-per-cpu=4000MB                # Request nGB RAM per core
-#SBATCH --array=0-5                         # distributed array job   
+##SBATCH --array=5001-10000                         # distributed array job   
 
 ## gpu
 ##SBATCH --partition=gpuq                    # the DGX only belongs in the 'gpu'  partition
@@ -21,5 +21,6 @@
 ##SBATCH --mem-per-cpu=16000MB               # memory per CORE; total memory is 1 TB (1,000,000 MB)
 
 ##nvidia-smi
-python generators/DownloadCleanFasta.py
+##python generators/DownloadCleanFasta.py
+python generators/data_helper.py
 ##python generators/Features.py
