bug solved regarding attn_mask computation

models/ContextTransformer.py
@@ -179,7 +179,7 @@ def train(model, optimizer, criterion, train_loader, device):
 
 def test(model, criterion, loader, device):
     model.eval()
-    loss_list, acc_list, precision_list, recall_list = [], [], [], []
+    loss_list, acc_list = [], []
     for data, y_true in loader:
         x, key_padding_mask, attn_mask = data["src"].to(device), data["key_padding_mask"].to(device), data["attn_mask"].to(device)
         attn_mask = torch.cat([i for i in attn_mask])
@@ -196,10 +196,8 @@ def test(model, criterion, loader, device):
         
         
         acc_list.append(accuracy_score(y_true, y_pred))
-        precision_list.append(precision_score(y_true, y_pred))
-        recall_list.append(recall_score(y_true, y_pred))
         
-    return np.mean(loss_list), np.mean(acc_list), np.mean(precision_list), np.mean(precision_list)
+    return np.mean(loss_list), np.mean(acc_list)
 
 
 # if __name__ == "__main__":

models/SCOPDataset.py
@@ -40,10 +40,14 @@ class SCOPDataset(Dataset):
         return out
 
     def get_attn_mask(self, contact_map):
-        attn_mask = torch.ones([self.max_len, self.max_len])
+        contact_map = torch.rand([230, 230])
+        contact_map = contact_map[:self.max_len, :self.max_len] #truncate to max_len
+        print(contact_map.shape)
+        attn_mask = torch.ones([self.max_len, self.max_len]) #necessary padding
         attn_mask[:contact_map.shape[0], :contact_map.shape[1]] = torch.tensor(contact_map)
         attn_mask = torch.logical_not(attn_mask.to(dtype=torch.bool))
         attn_mask = attn_mask.repeat(self.n_attn_heads, 1, 1)
+        print(attn_mask.shape)
         return attn_mask
 
 

models/train_test.py
@@ -70,14 +70,12 @@ if os.path.exists(f"outputs/models/{out_filename}.pth"):
 best_loss = np.inf
 for epoch in range(start_epoch, n_epochs+start_epoch):
     train_loss = ContextTransformer.train(model, optimizer, criterion, train_loader, device)
-    val_loss, avg_acc, avg_precision, avg_recall = ContextTransformer.test(model, criterion, val_loader, device)
+    val_loss, avg_acc = ContextTransformer.test(model, criterion, val_loader, device)
     crnt_lr = optimizer.param_groups[0]["lr"]
-    print(f"Epoch: {epoch:03d}, crnt_lr: {crnt_lr:.5f}, train loss: {train_loss:.4f}, val loss: {val_loss:.4f}, avg_acc: {avg_acc:.2f}, avg_precision: {avg_precision:.2f}, avg_recall: {avg_recall:.2f}")
+    print(f"Epoch: {epoch:03d}, crnt_lr: {crnt_lr:.5f}, train loss: {train_loss:.4f}, val loss: {val_loss:.4f}, avg_acc: {avg_acc:.2f}")
     writer.add_scalar('train loss',train_loss,epoch)
     writer.add_scalar('val loss',val_loss,epoch)
     writer.add_scalar('avg_acc',avg_acc,epoch)
-    writer.add_scalar('avg_precision',avg_precision,epoch)
-    writer.add_scalar('avg_recall',avg_recall,epoch)
     writer.add_scalar('crnt_lr',crnt_lr,epoch)
 
     # save model dict