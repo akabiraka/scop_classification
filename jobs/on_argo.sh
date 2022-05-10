31ae4bf1f0b4850686b04484276e28d3c55c4791
Anowarul Kabir <akabir0101@gmail.com>
2022-05-10 18:15:05 -0400

    runs updated

--- jobs/on_argo.sh
+++ jobs/on_argo.sh
@@ -3,9 +3,9 @@
 ## this must be run from directory where run.py exists.
 ## --workdir is not used in this file.
 
-#SBATCH --job-name=eval
-#SBATCH --output=/scratch/akabir4/scop_classification/outputs/argo_logs/eval-%j.out
-#SBATCH --error=/scratch/akabir4/scop_classification/outputs/argo_logs/eval-%j.err
+#SBATCH --job-name=scop
+#SBATCH --output=/scratch/akabir4/scop_classification/outputs/argo_logs/scop-%j.out
+#SBATCH --error=/scratch/akabir4/scop_classification/outputs/argo_logs/scop-%j.err
 #SBATCH --mail-user=<akabir4@gmu.edu>
 #SBATCH --mail-type=BEGIN,END,FAIL
 
@@ -20,5 +20,5 @@
 ##SBATCH --time=1-24:00
 
 ##nvidia-smi
-##python models/train_test.py
-python models/eval
+python models/train_test.py
+##python models/eval.py
