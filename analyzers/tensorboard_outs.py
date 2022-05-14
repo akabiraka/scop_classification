c75af0ff6ade62863c6de495b46a7a25bc64de4b
akabiraka <akabir0101@gmail.com>
2022-05-14 15:50:31 -0400

    new eval added

--- analyzers/tensorboard_outs.py
+++ analyzers/tensorboard_outs.py
@@ -1,10 +1,11 @@
 import sys
 sys.path.append("../scop_classification")
 
+import numpy as np
 import matplotlib.pyplot as plt
 from tensorboard.backend.event_processing import event_accumulator
 
-def parse_tensorboard(path, img_name=None, img_format="png"):
+def parse_tensorboard(path, model_name=""):
     ea = event_accumulator.EventAccumulator(
         path,
         size_guidance={event_accumulator.SCALARS: 0},
@@ -13,7 +14,7 @@ def parse_tensorboard(path, img_name=None, img_format="png"):
     # make sure the scalars are in the event accumulator tags
     # print(ea.Tags()["scalars"]) # shows the tags
     
-    labels = ["Train loss (best=0.0)", "Val loss (best=0.0)", "Accuracy (best=1.0)"]
+    labels=["Train loss", "Val loss", "Val accuracy"]
     for j, tag in enumerate(["train loss", "val loss", "acc"]):
         x, y=[],[]
         for i, event in enumerate(ea.Scalars(tag)):
@@ -22,12 +23,21 @@ def parse_tensorboard(path, img_name=None, img_format="png"):
             y.append(event.value)
             # plt.plot(event.step, event.value)
             # () 
-            if i==200: break
-        plt.plot(x, y, label=labels[j])
+            if i==175: break
+        if "loss" in tag: plt.plot(x, y, label=model_name + " " +labels[j] + f" (best={np.min(y):.3f})")
+        elif "acc" in tag: plt.plot(x, y, label=model_name + " " +labels[j] + f" (best={np.max(y):.3f})")
     plt.legend()
     plt.xlabel("Number of epochs")
     plt.ylabel("Cross-entropy loss/Accuracy")
-    if img_name==None: plt.show()
-    else: plt.savefig(f"outputs/images/{img_name}.{img_format}", dpi=300, format=img_format, bbox_inches='tight', pad_inches=0.0)
+    
 
-parse_tensorboard(path="outputs/tensorboard_runs/Model1_contactmap_SF_512_128_8_512_5_0.1_0.0001_1000_64_True_cuda/events.out.tfevents.1652397952.dgx002.orc.gmu.edu.1563475.0")
+parse_tensorboard(path="outputs/tensorboard_runs/Model1_contactmap_SF_512_128_8_512_5_0.1_0.0001_1000_64_True_cuda/events.out.tfevents.1652397952.dgx002.orc.gmu.edu.1563475.0",
+                  model_name="Our model")   
+
+parse_tensorboard(path="outputs/tensorboard_runs/CW_1e-05_64_300_cuda/events.out.tfevents.1652220562.node056.orc.gmu.edu.8499.0",
+                  model_name="PRoBERTa")                  
+
+img_name="our_model_vs_proberta_performance_comparison"
+img_format="png"
+if img_name==None: plt.show()
+else: plt.savefig(f"outputs/images/{img_name}.{img_format}", dpi=300, format=img_format, bbox_inches='tight', pad_inches=0.0)
--- models/eval.py
+++ models/eval.py
@@ -13,20 +13,20 @@ from torch.utils.data import DataLoader
 task="SF"
 max_len=512 #1024
 dim_embed=128 #512
-n_attn_heads=4 #16 #dim_embed must be divisible by num_head
+n_attn_heads=8 #16 #dim_embed must be divisible by num_head
 dim_ff=4*dim_embed 
-n_encoder_layers=3
+n_encoder_layers=5
 dropout=0.1
-init_lr=1e-5
+init_lr=1e-4
 n_epochs=1000 #1000 
 batch_size=64 #100
 start_epoch=1
 include_embed_layer=True
 attn_type="contactmap" #contactmap, nobackbone, longrange
 device = "cuda" if torch.cuda.is_available() else "cpu" # "cpu"#
-out_filename = f"CT_{attn_type}_{task}_{max_len}_{dim_embed}_{n_attn_heads}_{dim_ff}_{n_encoder_layers}_{dropout}_{init_lr}_{n_epochs}_{batch_size}_{include_embed_layer}_{device}"
+out_filename = f"Model1_{attn_type}_{task}_{max_len}_{dim_embed}_{n_attn_heads}_{dim_ff}_{n_encoder_layers}_{dropout}_{init_lr}_{n_epochs}_{batch_size}_{include_embed_layer}_{device}"
 print(out_filename)
-# CT_contactmap_SF_512_128_4_512_3_0.1_1e-05_1000_64_True_cuda
+# Model1_contactmap_SF_512_128_8_512_5_0.1_0.0001_1000_64_True_cuda
 
 
 all_data_file_path="data/splits/all_cleaned.txt"
@@ -49,12 +49,43 @@ checkpoint = torch.load(f"outputs/models/{out_filename}.pth")
 model.load_state_dict(checkpoint['model_state_dict'])
 
 
+@torch.no_grad()
+def test(model, criterion, loader, device):
+    model.eval()
+    losses, pred_labels, true_labels = [], [], []
+    for i, (data, y_true) in enumerate(loader):
+        x, key_padding_mask, attn_mask = data["src"].to(device), data["key_padding_mask"].to(device), data["attn_mask"].to(device)
+        attn_mask = torch.cat([i for i in attn_mask])
+        model.zero_grad(set_to_none=True)
+        y_pred = model(x, key_padding_mask, attn_mask)
+        loss = criterion(y_pred, y_true.to(device))
+        
+        losses.append(loss.item())
+        pred_labels.append(y_pred.argmax(dim=1).cpu().numpy())
+        true_labels.append(y_true.cpu().numpy())
+        
+    metrics = get_metrics(true_labels, pred_labels)
+    loss = np.mean(losses)
+    return loss, metrics
+
+
+def get_metrics(target_classes, pred_classes):
+    from sklearn.metrics import accuracy_score, recall_score, precision_score
+    acc = accuracy_score(target_classes, pred_classes)
+    precision = precision_score(target_classes, pred_classes, average="micro")
+    recall = recall_score(target_classes, pred_classes, average="micro")
+    return {"acc": acc, 
+            "precision": precision, 
+            "recall": recall, 
+            "pred_classes": pred_classes, 
+            "target_classes": target_classes}
+
 # evaluating validation set
 val_data_file_path="data/splits/val_4458.txt"
 val_dataset = SCOPDataset(val_data_file_path, class_dict, n_attn_heads, task, max_len, attn_type)
 val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 print(f"val data: {len(val_loader)}")
-val_loss, metrics = ContextTransformer.test(model, criterion, val_loader, device)
+val_loss, metrics = test(model, criterion, val_loader, device)
 print(f"Val: {val_loss}, {metrics}")
 
 # evaluating test set
@@ -62,5 +93,5 @@ test_data_file_path="data/splits/test_5862.txt"
 test_dataset = SCOPDataset(test_data_file_path, class_dict, n_attn_heads, task, max_len, attn_type)
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 print(f"val data: {len(test_loader)}")
-test_loss, metrics = ContextTransformer.test(model, criterion, test_loader, device)
-print(f"Test: {val_loss}, {metrics}")
+test_loss, metrics = test(model, criterion, test_loader, device)
+print(f"Test: {test_loss}, {metrics}")
